e72b3411d85c6f26650e963c306de059db906da3
Your Name <you@example.com>
2023-03-25 20:19:58 -0700

    xcode file

---
+++ tiktok-clone/tiktok-clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tiktok-clone
+//
+//  Created by Chima onyekwere on 3/25/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
